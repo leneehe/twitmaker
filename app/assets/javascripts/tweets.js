5187313dfcb03ce6d7c640a2d055023e3c0141ac
Lena <fatmeow@gmail.com>
2018-01-04 21:37:47 -0500

    Complete jquery ajax request with html response

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener('DOMContentLoaded', function(){
+  var form = document.querySelector('#new_tweet'),
+      tweetBtn = form.querySelector('#create-tweet'),
+      tweetMsg = form.querySelector('#tweet_message'),
+      ul = document.querySelector('ul.tweets');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: form.getAttribute('action'),
+      method: form.getAttribute('method'),
+      data: $(tweetMsg).serialize(),
+      dataType: 'html',
+    }).done(function(data) {
+      console.log(data)
+      $(ul).prepend( data)
+    })
+  })
+})
